907d59f32f3efebcbd9ff969dec46f96c563b901
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2024-05-28 19:45:08 +0530

    Create File
    Create File in Migration file

--- Backend/task_manager_app/todo/models.py
+++ Backend/task_manager_app/todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ToDo(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=500)
+    completed = models.BooleanField(default=False)
+    def __str__(self):
+        return self.title
